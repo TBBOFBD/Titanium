73164d32af5a36fce1120a0bac7b99d69dfb0000
Hypernite <drunkenmonkey4987@gmail.com>
2023-03-29 13:37:11 -0400

    Fix again, hopefully

--- titanium/desktop/keybinds/src/lib.rs
+++ titanium/desktop/keybinds/src/lib.rs
@@ -1,16 +1,3 @@
 //! Keybinds for Titanium
 
-#[doc(hidden)]
-mod common;
-#[doc(hidden)]
-mod public;
-pub use crate::public::*;
-
-#[cfg(target_os = "windows")]
-mod windows;
-#[cfg(target_os = "windows")]
-pub use crate::windows::*;
-#[cfg(target_os = "linux")]
-mod linux;
-#[cfg(target_os = "linux")]
-pub use crate::linux::*;
+pub use inputbot::*;
